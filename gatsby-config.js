b9d665da0ed561edf62ad8edbc9509194061a6eb
peter_vida1 <peter_vida1@epam.com>
2023-06-05 14:20:22 +0200

    pre deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ require("dotenv").config({
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/POC-language-prac",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
